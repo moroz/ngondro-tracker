db5e33afda722b0623873637f364d91c5fe64fd7
Karol Moroz <karol@moroz.dev>
2022-06-03 00:44:21 +0200

    Initial Commit

---
+++ Ngondro Tracker/Ngondro_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ngondro_TrackerApp.swift
+//  Ngondro Tracker
+//
+//  Created by Karol Moroz on 03/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct Ngondro_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
